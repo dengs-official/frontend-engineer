0f15ff7f9cfc141ab219b01dc5cb286e23915558
dengs.official <dengs.official@gmail.com>
2021-09-03 11:57:43 +0800

    test(github): git提交用户信息测试

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import Counter from './Counter';
 import Bag from './DataStructure/Bag';
 
-const heads = new Counter('heads');
-heads.increment();
 const bags = new Bag<number>();
 bags.add(1);
 bags.add(2);
 bags.add(3);
 bags.add(4);
+
 for (const item of bags) {
   if (item > 3) {
     break;
