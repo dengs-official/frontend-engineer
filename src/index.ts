9405ae303181cb8b6bb8acde7a07e90179c0e5cf
邓林华 <deng.linhua@datasw.com>
2021-09-03 10:13:44 +0800

    tests(index): 添加测试用例

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('');
+import Counter from './Counter';
+import Bag from './DataStructure/Bag';
+
+const heads = new Counter('heads');
+heads.increment();
+const bags = new Bag<number>();
+bags.add(1);
+bags.add(2);
+bags.add(3);
+bags.add(4);
+for (const item of bags) {
+  if (item > 3) {
+    break;
+  }
+  console.log('first ' + item);
+}
+for (const item of bags) {
+  console.log('second ' + item);
+}
