67bcbae2ca04055bc73ee635f4987e406bbc6e84
dengs.official <dengs.official@gmail.com>
2021-09-15 22:12:32 +0800

    docs(binarySearch): 添加二分查找说明

--- src/searchAlgorithm/binarySearch.ts
+++ src/searchAlgorithm/binarySearch.ts
@@ -4,13 +4,15 @@
  *
  * 时间复杂度O(logN)
  *
+ * 递归思想，每次将数组二分，判断当前数在哪边范围，在之内继续查找
+ *
  * @param array
  * @param target
  * @returns
  */
 export default function binarySearch(array: number[], target: number): [boolean, number] {
-  let index = (function find(start: number, end: number): number | undefined {
-    const mid = start + Math.ceil((end - start) >> 1);
+  let index = (function process(start: number, end: number): number | undefined {
+    const mid = start + ((end - start) >> 1);
     if (array[mid] === target) {
       return mid;
     }
@@ -20,7 +22,7 @@ export default function binarySearch(array: number[], target: number): [boolean,
       end = mid - 1;
     }
     if (start <= end) {
-      return find(start, end);
+      return process(start, end);
     }
   })(0, array.length - 1);
 
