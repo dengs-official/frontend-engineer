ab17fa2f8dfa9caa91f122233a40909441fc8071
dengs.official <dengs.official@gmail.com>
2021-10-20 21:01:47 +0800

    feat(Node.js): 更新事件模块

--- Node.js/Introduction/07.events.js
+++ Node.js/Introduction/07.events.js
@@ -6,11 +6,29 @@ const EventEmitter = require("events");
 const eventEmitter = new EventEmitter();
 
 let number = 0;
+eventEmitter.once("once", (number) => {
+  console.log("Once:" + number);
+});
 eventEmitter.on("start", (number) => {
-  console.log("开始" + number);
+  console.log("开始1:" + number);
+});
+eventEmitter.addListener("start", (number) => {
+  console.log("开始2:" + number);
+});
+eventEmitter.prependListener("start", (number) => {
+  console.log("开始3:" + number);
 });
 
+console.log(eventEmitter.eventNames());
+console.log(eventEmitter.listenerCount("start"));
+console.log(eventEmitter.listeners("start"));
+console.log(eventEmitter.getMaxListeners());
+
 eventEmitter.emit("start", ++number);
+eventEmitter.emit("once", number);
+// eventEmitter.off("start", () => {});
+eventEmitter.removeAllListeners("start");
 setTimeout(() => {
   eventEmitter.emit("start", ++number);
+  eventEmitter.emit("once", number);
 }, 1000);
