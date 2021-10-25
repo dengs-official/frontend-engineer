b8176e1ff862d00c6a2db0a297b76fe54a512c53
dengs.official <dengs.official@gmail.com>
2021-10-25 23:47:58 +0800

    feat(Webpack Introduction): 拆分不同模式

--- BuildTool/Webpack/introduction/webpack.config.js
+++ BuildTool/Webpack/introduction/webpack.config.js
@@ -1,26 +1,42 @@
 const path = require("path");
+const merge = require("webpack-merge").merge;
 const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const resolve = (file) => path.resolve(__dirname, file);
 
-module.exports = {
-  // mode: "production",
-  entry: "./src/main.js",
-  output: {
-    path: resolve("dist"),
-    filename: "[name].[hash].js",
-  },
-  resolve: {
-    alias: {
-      "@": resolve("src/"),
+// module.exports = {
+module.exports = (env, argv) => {
+  const config = argv.mode === "development" ? devConfig : prodConfig;
+  return merge(baseConfig(env, argv), config);
+};
+
+const baseConfig = (env, argv) => {
+  return {
+    entry: "./src/main.js",
+    output: {
+      path: resolve("dist"),
+      filename: "[name].[hash].js",
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: resolve("public/index.html"),
-      favicon: resolve("public/favicon.svg"),
-    }),
-  ],
+    resolve: {
+      alias: {
+        "@": resolve("src/"),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve("public/index.html"),
+        favicon: resolve("public/favicon.svg"),
+      }),
+    ],
+  };
+};
+
+const devConfig = {
+  mode: "development",
+};
+
+const prodConfig = {
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
 };
