c911509a3fdb75fc4b6fd38ecb8ae8fefde7c158
dengs.official <dengs.official@gmail.com>
2021-10-26 15:02:37 +0800

    feat(Webpack Introduction): 添加样式loader

--- BuildTool/Webpack/introduction/webpack.config.js
+++ BuildTool/Webpack/introduction/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const merge = require("webpack-merge").merge;
 const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const resolve = (file) => path.resolve(__dirname, file);
 
@@ -12,16 +13,25 @@ module.exports = (env, argv) => {
 };
 
 const baseConfig = (env, argv) => {
+  const devMode = argv.mode === "development";
   return {
     entry: "./src/main.js",
     output: {
       path: resolve("dist"),
-      filename: "[name].[hash].js",
+      filename: "[name].[fullhash].js",
     },
     resolve: {
       alias: {
         "@": resolve("src/"),
       },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -38,5 +48,10 @@ const devConfig = {
 
 const prodConfig = {
   mode: "production",
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[fullhash].css",
+    }),
+  ],
 };
