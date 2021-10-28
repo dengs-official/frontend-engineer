235b96085e65a009d74c11686eeccdcc9563f365
dengs.official <dengs.official@gmail.com>
2021-10-28 22:25:15 +0800

    feat(Reack Pack): 初始化react 和 webpack项目，完成基础配置

---
+++ BuildTool/Webpack/react-pack/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "@/assets/logo.svg";
+function App() {
+  return (
+    <div className="App">
+      <header>App</header>
+      <main>
+        <img src={logo} className="App-logo" alt="logo" />
+      </main>
+    </div>
+  );
+}
+export default App;
---
+++ BuildTool/Webpack/react-pack/webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const merge = require("webpack-merge").merge;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const resolve = (file) => path.resolve(file);
+
+module.exports = (env, argv) => {
+  const config = argv.mode === "production" ? prodConfig : devConfig;
+  return merge(baseConfig(env, argv), config);
+};
+const baseConfig = (env, argv) => {
+  const prodMode = argv.mode === "production";
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: resolve("dist"),
+      filename: "js/[name].[fullhash].js",
+      clean: true,
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      alias: {
+        "@": resolve("src"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)/,
+          include: /src/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.(sa|sc|c)ss/,
+          include: /src/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "img/[name].[hash].[ext][query]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve("public/index.html"),
+        inject: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[fullhash].css",
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "all",
+          },
+        },
+      },
+    },
+  };
+};
+
+const devConfig = {
+  mode: "development",
+};
+const prodConfig = {
+  mode: "production",
+  plugins: [new BundleAnalyzerPlugin({ analyzerMode: "static", openAnalyzer: false })],
+};
