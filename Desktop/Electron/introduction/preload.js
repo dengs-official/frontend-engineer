aed462c5ec9da46df399eb14ede9d46c9cb12bf8
dengs.official <dengs.official@gmail.com>
2021-11-04 22:44:25 +0800

    feat(Electron Introduction): 添加dark mode

--- Desktop/Electron/introduction/preload.js
+++ Desktop/Electron/introduction/preload.js
@@ -1,3 +1,9 @@
+const { contextBridge, ipcRenderer } = require("electron");
+contextBridge.exposeInMainWorld("darkMode", {
+  toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
+  system: () => ipcRenderer.invoke("dark-mode:system"),
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceTextFn = (selector, text) => {
     const element = document.getElementById(selector);
