436f0c82752b667cddc8ef47a0a38e341cab4c26
dengs.official <dengs.official@gmail.com>
2021-11-21 16:24:58 +0800

    feat(netease-music): 初始化项目，包括electron，wbpack

---
+++ Desktop/Electron/netease-music/src/main/index.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { app, BrowserWindow } from "electron";
+
+const r = (file: string): string => path.resolve(__dirname, file);
+
+let mainWin: BrowserWindow;
+
+function createWindow(): void {
+  mainWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: r("preload.js"),
+    },
+  });
+  mainWin.loadFile(r("../renderer/index.html"));
+}
+
+app.whenReady().then(() => createWindow());
+
+if (process.env.DEBUG === "true") {
+  app.commandLine.appendSwitch("remote-debugging-port", "9222");
+}
